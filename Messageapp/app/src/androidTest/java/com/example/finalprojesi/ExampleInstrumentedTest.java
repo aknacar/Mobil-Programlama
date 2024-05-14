74515a56f45ecf2474309df8001b127133cefe1a
acar104 <147639708+aknacar@users.noreply.github.com>
2024-05-14 11:07:24 +0300

    MessageApp

--- Messageapp/app/src/androidTest/java/com/example/finalprojesi/ExampleInstrumentedTest.java
+++ Messageapp/app/src/androidTest/java/com/example/finalprojesi/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.messageapp;
+package com.example.finalprojesi;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.messageapp", appContext.getPackageName());
+        assertEquals("com.example.finalprojesi", appContext.getPackageName());
     }
 }
